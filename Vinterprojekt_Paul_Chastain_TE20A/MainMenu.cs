1f81fbfceefe7cb90eb3d8ba1227a7550e693477
Paul Chastain <paulchast1@gmail.com>
2022-12-11 19:05:42 +0100

    main menu wip

--- Vinterprojekt_Paul_Chastain_TE20A/MainMenu.cs
+++ Vinterprojekt_Paul_Chastain_TE20A/MainMenu.cs
@@ -1,13 +1,78 @@
 using Raylib_cs;
 
-public class MainMenu : Menu
+public class MainMenu
 {
-    // protected List<(Rectangle, string)> buttons;
+    Rectangle play = new Rectangle(Raylib.GetScreenWidth() / 2 - 100, 250, 200, 100);
+    Rectangle howToPlay = new Rectangle(Raylib.GetScreenWidth() / 2 - 100, 400, 200, 100);
+    Rectangle exit = new Rectangle(Raylib.GetScreenWidth() / 2 - 100, 550, 200, 100);
 
-    public MainMenu()
+    public void Menu()
     {
-        Rectangle button = new Rectangle(250, 300, 200, 100);
+        while (!Raylib.WindowShouldClose() /*&& quit == false*/)
+        {
+            Raylib.BeginDrawing();
 
-        buttons.Add(new(button, "Start"));
+            Raylib.ClearBackground(Color.SKYBLUE);
+            Raylib.DrawText("Deep Scan", Raylib.GetScreenWidth() / 2 - 80, 225, 100, Color.RED);
+
+            Raylib.DrawRectangleRec(play, Color.);
+            Raylib.DrawText("Start", Raylib.GetScreenWidth() / 2 - 80, 375, 50, Color.WHITE);
+
+            Raylib.DrawRectangleRec(exit, vars.exitColor);
+            Raylib.DrawText("Exit", Raylib.GetScreenWidth() / 2 - 50, 525, 50, Color.WHITE);
+
+            Raylib.EndDrawing();
+
+            // Kollar om muspekaren är på en knapp och ändrar färgen om den är på en knapp
+            if (Raylib.GetMouseX() >= Raylib.GetScreenWidth() / 2 - 160 && Raylib.GetMouseX() <= Raylib.GetScreenWidth() / 2 + 140 && Raylib.GetMouseY() >= 350 && Raylib.GetMouseY() <= 450)
+            {
+                vars.playColor = Color.DARKBLUE;
+
+                //Laddar in spelet om man klickar på "start" knappen
+                if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                {
+                    vars.gameStart = true;
+                    vars.menu = false;
+                    return;
+                }
+            }
+            else
+            {
+                vars.playColor = Color.BLUE;
+            }
+
+            if (Raylib.GetMouseX() >= Raylib.GetScreenWidth() / 2 - 160 && Raylib.GetMouseX() <= Raylib.GetScreenWidth() / 2 + 140 && Raylib.GetMouseY() >= 500 && Raylib.GetMouseY() <= 600)
+            {
+                vars.exitColor = Color.DARKBLUE;
+
+                // Stänger ner spelet om man trycker på "exit knappen"
+                if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                {
+                    quit = true;
+                    System.Console.WriteLine(quit);
+                    return;
+                }
+            }
+            else
+            {
+                vars.exitColor = Color.BLUE;
+            }
+        }
     }
-}
+    // protected List<(Rectangle, string)> buttons
+
+    // public void CreateButtons()
+    // {
+    //     buttons.Add(new(new(250, 300, 200, 100), "Start"));
+    //     buttons.Add(new(new(400, 300, 200, 100), "Start"));
+    //     buttons.Add(new(new(550, 300, 200, 100), "Exit"));
+    // }
+
+    // public override void MenuButtons()
+    // {
+    //     Raylib.BeginDrawing();
+    //     Raylib.ClearBackground(Color.SKYBLUE);
+    //     base.MenuButtons();
+    //     Raylib.EndDrawing();
+    // }
+}   
--- Vinterprojekt_Paul_Chastain_TE20A/Menu.cs
+++ Vinterprojekt_Paul_Chastain_TE20A/Menu.cs
@@ -1,6 +1,15 @@
 using Raylib_cs;
 
-public abstract class Menu
-{
-    protected List<(Rectangle rect, string name)> buttons;
-}
+// public abstract class Menu
+// {
+//     protected List<(Rectangle rect, string name)> buttons = new List<(Rectangle rect, string name)>();
+
+//     public virtual void MenuButtons()
+//     {
+//         foreach (var item in buttons)
+//         {
+//             Raylib.DrawRectangleRec(item.rect, Color.RED);
+//         }
+//     }
+// }
+
