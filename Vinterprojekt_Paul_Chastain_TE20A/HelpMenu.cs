c53287f3f9225bc2948d31b17fa096e18f1b647b
Paul Chastain <paulchast1@gmail.com>
2022-12-15 16:00:33 +0100

    more screens WIP

--- Vinterprojekt_Paul_Chastain_TE20A/HelpMenu.cs
+++ Vinterprojekt_Paul_Chastain_TE20A/HelpMenu.cs
@@ -1,9 +1,16 @@
-using System;
+using Raylib_cs;
 
 public class HelpMenu
 {
+    LoadScreen l = new();
+
     public void HelpScreen()
     {
-        
-    }    
+        while (!Raylib.WindowShouldClose() && l.help == true)
+        {
+            Raylib.BeginDrawing();
+            
+            Raylib.EndDrawing();
+        }
+    }
 }
