2ba70abc0c8468c4055eae5e51bb3c69ae739652
Paul Chastain <paulchast1@gmail.com>
2023-04-19 13:04:53 +0200

    changed enums "depth" & "side" to dictionaries

--- Vinterprojekt_Paul_Chastain_TE20A/Entities/Enemy.cs
+++ Vinterprojekt_Paul_Chastain_TE20A/Entities/Enemy.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using Raylib_cs;
 
 public class Enemy
@@ -6,8 +8,8 @@ public class Enemy
     public static int CurrentAmount { get; set; } = 0;
     public int Pos { get; set; }
     protected int SpawnSide { get; set; }
-    protected Depth Depths { get; set; }
-    protected Side Sides { get; set; }
+    protected int Depths { get; set; }
+    protected int Sides { get; set; }
     protected bool LeftSide { get; set; }
     public Rectangle enemyHitbox;
     protected Image submarineIMG = Raylib.LoadImage(@"Images/submarine.png");
@@ -19,50 +21,46 @@ public class Enemy
 
     public static List<Enemy> spawnedEnemies = new(); // En lista med alla fiender på skärmen
 
-    // Y-koordinaterna som ubåtarna kan spawna på.
-    public enum Depth
-    {
-        one = 150,
-        two = 250,
-        three = 350,
-        four = 450,
-        five = 550,
-        six = 650
-    }
-
-    // X-koordinaterns som ubåtarna kan spawna på.
-    public enum Side
-    {
-        left = -200,
-        right = 800
-    }
-
     public Enemy()
     {
+        // Y-koordinaterna som ubåtarna kan spawna på.
+        Dictionary<string, int> depth = new Dictionary<string, int>();
+        depth.Add("one", 150);
+        depth.Add("two", 250);
+        depth.Add("three", 350);
+        depth.Add("four", 450);
+        depth.Add("five", 550);
+        depth.Add("six", 650);
+
+        // X-koordinaterns som ubåtarna kan spawna på.
+        Dictionary<string, int> side = new Dictionary<string, int>();
+        side.Add("left", -200);
+        side.Add("right", 800);
+
         int depthLevel = rdm.Next(6);
         int spawnSide = rdm.Next(2);
 
 
         // Anger vilken höjd ubåten ska spawna på baserat på det slumpade värdet.
-        if (depthLevel < 1) Depths = Depth.one;
-        else if (depthLevel < 2) Depths = Depth.two;
-        else if (depthLevel < 3) Depths = Depth.three;
-        else if (depthLevel < 4) Depths = Depth.four;
-        else if (depthLevel < 5) Depths = Depth.five;
-        else if (depthLevel < 6) Depths = Depth.six;
+        if (depthLevel < 1) Depths = depth["one"];
+        else if (depthLevel < 2) Depths = depth["two"];
+        else if (depthLevel < 3) Depths = depth["three"];
+        else if (depthLevel < 4) Depths = depth["four"];
+        else if (depthLevel < 5) Depths = depth["five"];
+        else if (depthLevel < 6) Depths = depth["six"];
 
         // Anger vilken sida ubåten ska spawna på, samt "flippar" bilden horizontelt
         // om den spawnar på vänster sida.
         if (spawnSide == 0) 
         {
-            Sides = Side.left;
+            Sides = side["left"];
             LeftSide = true;
             Raylib.ImageFlipHorizontal(ref submarineIMG);
             Raylib.ImageFlipHorizontal(ref dartIMG);
         }
         else if(spawnSide == 1) 
         {
-            Sides = Side.right;
+            Sides = side["right"];
             LeftSide = false;
         }
         Pos = (int) Sides;
